190b021660712e5f11c1e2e33c170400132161c8
videotronorg <afhbny29@gmail.com>
2025-01-20 04:19:01 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const TELEGRAM_TOKEN = "6968043726:AAHGJ0OtlbTMjTOPTfacioSJqOwgePt0Xhw";
-export const CHAT_ID = 5710607863;
+export const TELEGRAM_TOKEN = "6567283982:AAEnrAsI1zzaLrGFULyuw-FArp4XMwJbNak";
+export const CHAT_ID = 6629907685;
 export const IP_TOKEN = "34a50f68edba06";
